8c3635800095026238a5f20272fdd73b52be7cdd
Linda "zig" Speight <linda@reachmaine.com>
2017-05-08 16:11:41 -0400

    Dont show other shipping methods when free shipping is available.

--- owlstool/custom/woocommerce.php
+++ owlstool/custom/woocommerce.php
@@ -41,7 +41,26 @@ add_filter( 'woocommerce_shipping_free_shipping_is_available', 'my_free_shipping
 
 // to remove sku from everywhere....
 add_filter( 'wc_product_sku_enabled', '__return_false' );
-
+//
+/**
+ * Hide shipping rates when free shipping is available.
+ * Updated to support WooCommerce 2.6 Shipping Zones.
+ *
+ * @param array $rates Array of rates found for the package.
+ * @return array
+ */
+function my_hide_shipping_when_free_is_available( $rates ) {
+	$free = array();
+	foreach ( $rates as $rate_id => $rate ) {
+		if ( 'free_shipping' === $rate->method_id ) {
+			$free[ $rate_id ] = $rate;
+			break;
+		}
+	}
+	return ! empty( $free ) ? $free : $rates;
+}
+add_filter( 'woocommerce_package_rates', 'my_hide_shipping_when_free_is_available', 100 );
+//
 
 add_action('woocommerce_before_add_to_cart_form', 'display_free_shipping_text');
 function display_free_shipping_text() {
